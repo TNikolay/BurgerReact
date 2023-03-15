bf1bae4d077bbd96968083c05cab16561cbe34bd
Nikolay Tyushkov <begemot@begemotov.net>
2023-03-15 16:53:08 +0300

    feat: step 2

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,6 @@
-import style from './Container.module.css'
-import classNames from 'classnames'
-export const Container = ({ children, className }) => {
-    return (
-      <div className={classNames(style.container, className)}>
-        {children}
-      </div>
-    );
-}
+import style from './Container.module.css';
+import classNames from 'classnames';
+
+export const Container = ({ children, className }) => (
+  <div className={classNames(style.container, className)}>{children}</div>
+);
